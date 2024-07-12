f260dfa31488468a83f6e36472c5cb9fe65f4f5b
BooJunhyuk <boojh0811@gmail.com>
2024-07-12 16:47:15 +0900

    feat:userpart

--- step03_miniProject/src/hr_manager/service/UserService.java
+++ step03_miniProject/src/hr_manager/service/UserService.java
@@ -1,5 +1,39 @@
 package hr_manager.service;
 
+
+import java.util.ArrayList;
+import hr_manager.model.dto.User;
+
 public class UserService {
+	private static UserService userServiceInstance = new UserService();
+	
+	private UserService() {}
+	
+	public static UserService getUserServiceInstance() {
+		return userServiceInstance;
+	}
+	//User 저장
+	private ArrayList<User> UserList = new ArrayList<User>();
+	
+	/*
+	 *  모든 User
+	 *  @return 모든 UserInfo
+	 */
+	public ArrayList<User> getUserList(){
+		return UserList;
+	}
+	
+	/*
+	 * 로그인한 user email로 검색
+	 */
+	public User getUser(String email) {
+		for (User user : UserList) {
+			if (user != null && user.getEmail().equals(email)) {
+				return user;
+				//유저 존재 여부 확인 && 존재하는유저 email 반환 후 parameter 로 유입된 email과 비교
+			}
+		}
+		return null;
+	}
 
 }
--- step03_miniProject/src/hr_manager/view/UserView.java
+++ step03_miniProject/src/hr_manager/view/UserView.java
@@ -1,13 +1,60 @@
 package hr_manager.view;
 
+import java.util.Scanner;
+
+import hr_manager.controller.UserController;
+
 public class UserView {
 	
 	private String email;
 	
-	public UserView(String email) {
-		
-		System.out.println("UserView is called");
-		System.out.println(email);
+    UserController controller = UserController.getUserInstance();
+
+    public UserView(String email) {
+        this.email = email;
+    }
+
+
+	public void showMenu() {
+		Scanner sc = new Scanner(System.in);
+		Boolean flag = true;
+		while (flag) {
+			System.out.println("--- Menu ---");
+			System.out.println("(1) 전체 사용자 보기");
+			System.out.println("(2) 특정 사용자 검색");
+			System.out.println("(3) 현재 사용자 보기");
+			System.out.println("(4) 종료");
+			int i = sc.nextInt();
+			switch (i) {
+			case 1: {
+				controller.getUserList();
+				break;
+			}
+			case 2: {
+				System.out.println("--- 사용자 정보 입력 ---");
+
+				System.out.println("Email: ");
+				String email = sc.next();
+
+				controller.getUser(email);
+				break;
+			}
+			case 3: {
+                controller.getUser(this.email);
+                break;
+            }
+			default:
+				System.out.println("etc");
+				flag = false;
+				break;
+			}
+		}
 	}
-	
+
+//	public UserView(String email) {
+//
+//		System.out.println("UserView is called");
+//		System.out.println(email);
+//	}
+
 }
